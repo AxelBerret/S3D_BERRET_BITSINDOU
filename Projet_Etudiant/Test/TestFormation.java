33d2b720a1c03dc41d4085eab8079f93aeb5dc71
Axel Berret <axelberret6@etu.univ-lorraine.fr>
2023-09-26 16:18:37 +0200

    edit classe TestFormation

--- Projet_Etudiant/Test/TestFormation.java
+++ Projet_Etudiant/Test/TestFormation.java
@@ -1,10 +1,17 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class TestFormation {
 
-    public void setup() {
+    private Formation formation;
 
+    @BeforeEach
+    public void setup() {
+        formation = new Formation(5);
     }
 
 
+    @Test
     public void test_01_() {
 
     }
--- Projet_Etudiant/Test/TestIdentite.java
+++ Projet_Etudiant/Test/TestIdentite.java
@@ -1,11 +1,22 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestIdentite {
 
-    public void setup() {
+    private Identite id;
 
+    @BeforeEach
+    public void setup() {
+        id = new Identite("1", "Walczak", "Dimitri");
     }
 
 
-    public void test_01_() {
-
+    @Test
+    public void test_01_constructeur() {
+        assertEquals("1", id.getNIP());
+        assertEquals("Walczak", id.getNom());
+        assertEquals("Dimitri", id.getPrenom());
     }
 }
